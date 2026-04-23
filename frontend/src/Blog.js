6fcda78a7b5c485dd6a06fa2dc5696fddca30b8a
Shiva <vaddishiva51@gmail.com>
2026-04-23 15:34:20 +0530

    Update API URL default value to an empty string

--- frontend/src/Blog.js
+++ frontend/src/Blog.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
-const API = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API = process.env.REACT_APP_API_URL || " ";
 
 function timeAgo(iso) {
   const diff = Date.now() - new Date(iso).getTime();
